6ced7130f14274cb225a7182754353f7004fb852
jacktaoyang <jacktaoyang@tencent.com>
2023-12-19 16:45:26 +0800

    feat:add set & tree & sort

--- go.mod
+++ go.mod
@@ -18,7 +18,8 @@ require (
 )
 
 require (
-	github.com/BurntSushi/toml v1.2.0 // indirect
+	git.woa.com/khaos/eros v1.19.1-0.20231204125501-82bb10918760 // indirect
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/bytedance/sonic v1.10.0-rc2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
@@ -57,15 +58,16 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/kr/text v0.1.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
-	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/client_golang v1.15.0 // indirect
+	github.com/prometheus/client_golang v1.15.1 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/soheilhy/cmux v0.1.5 // indirect
+	github.com/tealeg/xlsx v1.0.5 // indirect
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20220101234140-673ab2c3ae75 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,64 @@ package main
 
 import (
 	"fmt"
-	ath "learn-service/algorithm/data_struct"
+	"github.com/gogf/gf/v2/text/gregex"
+	"github.com/tealeg/xlsx"
+	"log"
+	"strings"
 )
 
 func main() {
-	ath.Tower(3, "a", "b", "c")
-	fmt.Println(ath.Total)
+	instanceNames := []string{"实例", "instance", "实例_test_1", "_实例-1", "instance_test", "实例-", "-"}
+	for _, instance := range instanceNames {
+		log.Printf("'%v' is matched or not : %v", instance, regulerMatch(instance))
+	}
+}
+func regulerMatch(instanceName string) bool {
+	return gregex.IsMatchString(`[\p{Han}_a-zA-Z0-9\-]{1,60}`, instanceName)
+}
+func dealParamStrValue(val string) string {
+	target := val
+	if strings.Contains(val, "'") {
+		target = strings.Split(val, "'")[1]
+	}
+	log.Println(target)
+	return target
+}
+
+func Excel2SQL() {
+	xlFile, err := xlsx.OpenFile("proxy_params.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	insertStmt := "INSERT into default_param_config(default_template_id, component, param_path,param_name, param_type, default_value,enum_value, enum_split, max_value,min_value, created_at, updated_at) values "
+
+	// 获取第一个工作表
+	sheet := xlFile.Sheets[0]
+	// 遍历每一行
+	for i, row := range sheet.Rows {
+		// 跳过表头行
+		if i == 0 {
+			continue
+		}
+		// 生成单条插入语句的值部分
+		values := "('ptpl-9m4jn9f5', 'proxy',"
+		// 遍历每一列
+		for index, cell := range row.Cells {
+			text := cell.String()
+			if index == 0 {
+				values += fmt.Sprintf("'GateWay.%s', ", text)
+			} else {
+				values += fmt.Sprintf("'%s', ", text) // 将每个单元格的值添加到插入语句中
+			}
+		}
+		//// 去除最后一个逗号和空格
+		//values = values[:len(values)-2]
+		// 添加单条插入语句的值部分到批量插入语句中
+		insertStmt += values + "now(),now()), "
+	}
+
+	// 去除最后一个逗号和空格
+	insertStmt = insertStmt[:len(insertStmt)-2]
+	fmt.Println(insertStmt + ";")
 }
